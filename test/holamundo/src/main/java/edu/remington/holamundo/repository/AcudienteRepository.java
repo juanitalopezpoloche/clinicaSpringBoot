4cf36fbc499d04181eee99a27209005520a42529
Juanita <juanitalopezpoloche.13@gmail.com>
2026-02-18 19:16:48 -0500

    Creación del repositorio del acudiente

---
+++ test/holamundo/src/main/java/edu/remington/holamundo/repository/AcudienteRepository.java
@@ -0,0 +1,9 @@
+package edu.remington.holamundo.repository;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import edu.remington.holamundo.model.Acudiente;
+
+
+public interface AcudienteRepository extends JpaRepository<Acudiente, Long>{
+    Optional<Acudiente> findByDocumento(String documento);
+}
